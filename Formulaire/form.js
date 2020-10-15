16b9446d0c44f1fd2bf8f49267b5258d51995b57
AndryRaza <70998539+AndryRaza@users.noreply.github.com>
2020-10-15 15:00:34 +0400

    Add files via upload

--- Formulaire/form.js
+++ Formulaire/form.js
@@ -3,8 +3,17 @@ window.onload = function () {
 }
 
 function validation_bordure(){
-    $('input[type="text"]').each(function(){
+    $('input').not($('input[type="radio"]')).each(function(){
         if ($(this).val() == '') {
+           $(this).css('box-shadow','0 0 1px 2px red');
+        } 
+        else {
+            $(this).css('box-shadow','0 0 1px 2px green');
+        }
+    })
+    
+    $('select').each(function(){
+        if ($(this).val() == '' || $(this).val() == 'Choose option') {
            $(this).css('box-shadow','0 0 1px 2px red');
         } 
         else {
